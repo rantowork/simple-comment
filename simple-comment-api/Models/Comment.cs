e4ab4c98d8852f9fbf808eb8ca1a8177252230e8
Tim Randall <Tim@milyli.net>
2023-09-25 13:54:29 -0500

    feat: Add comment model.

---
+++ simple-comment-api/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace simple_comment_api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string? User { get; set; }
+        public string? CommentText { get; set; }
+        public string? Email { get; set; }
+    }
+}
