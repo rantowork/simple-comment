7ac88826dcd477308787d5c83e6da473117d973e
Tim Randall <Tim@milyli.net>
2023-10-09 16:00:59 -0500

    feat: Add web project!

---
+++ simple-comment-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace simple_comment_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
